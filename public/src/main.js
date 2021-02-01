7840ce1613e1c19e734b58679d06e7ff120a6a4d
Jon Telles <jongtelles@gmail.com>
2021-02-01 00:14:45 -0500

    working boilerplate

--- public/src/main.js
+++ public/src/main.js
@@ -1,26 +1,10 @@
 // export approach
 import Phaser from "./lib/phaser.js";
+import PlayGame from "./scenes/PlayGame.js";
 
-// import PlayGame from "./scenes/PlayGame";
-
-// export default new Phaser.Game({
-//   width: 480,
-//   height: 640,
-//   backgroundColor: "#ce5e82",
-//   scene: PlayGame,
-// });
-
-//onload
-let game;
-
-window.onload = () => {
-  const gameConfig = {
-    width: 480,
-    height: 640,
-    backgroundColor: "#ce5e82",
-    scene: PlayGame,
-  };
-
-  game = new Phaser.Game(gameConfig);
-  window.focus();
-};
+export default new Phaser.Game({
+  width: 480,
+  height: 640,
+  backgroundColor: "#ce5e82",
+  scene: PlayGame,
+});
